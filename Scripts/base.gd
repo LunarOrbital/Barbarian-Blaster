97605ceece65b0a92112712293057462f1e4b89b
LunarOrbital <254162809+LunarOrbital@users.noreply.github.com>
2026-01-20 11:57:47 -0600

    base added

---
+++ Scripts/base.gd
@@ -0,0 +1,5 @@
+extends Node3D
+class_name base
+
+func take_damage() -> void:
+	print("damage take")
--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,6 +1,7 @@
 extends PathFollow3D
 ##The speed in m/s the enemy will move
 @export var speed := 10
+@onready var base2 : base = get_tree().get_first_node_in_group("base")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +9,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress += speed *delta
+	if (progress_ratio >=1):
+		base2.take_damage()
+		queue_free()
+	else:
+		progress += speed *delta
