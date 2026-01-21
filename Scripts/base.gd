62607305801a74a35886ca60317e3928e757d471
LunarOrbital <254162809+LunarOrbital@users.noreply.github.com>
2026-01-21 12:10:36 -0600

    added label

--- Scripts/base.gd
+++ Scripts/base.gd
@@ -1,5 +1,16 @@
 extends Node3D
 class_name base
+@onready var label_3d: Label3D = $Label3D
+@export var max_health := 5
+var health : int :
+	set(new_health):
+		health = new_health
+		label_3d.text = str(health) + " / " + str(max_health)
+func _ready() -> void:
+	health = max_health
 
 func take_damage() -> void:
+	health -=1
 	print("damage take")
+	if (health < 0):
+		get_tree().reload_current_scene()
