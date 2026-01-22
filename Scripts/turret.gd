923bf3f554f077a7308cca96c9ab415190c214bd
LunarOrbital <254162809+LunarOrbital@users.noreply.github.com>
2026-01-22 12:41:02 -0600

    turret aiming

--- Scripts/turret.gd
+++ Scripts/turret.gd
@@ -2,6 +2,13 @@ extends Node3D
 @export var proj: PackedScene 
 @export var time := 1.0
 @onready var timer: Timer = $Timer
+@onready var enemy_path : Path3D
+
+#turn and look at enemy
+func  _physics_process(delta: float) -> void:
+	var enemy = enemy_path.get_children().back()
+	look_at(enemy.global_position, Vector3.UP, true)
+
 func _on_timer_timeout() -> void:
 	timer.wait_time = time
 	var newProj = proj.instantiate()
--- turret_manager.gd
+++ turret_manager.gd
@@ -1,8 +1,9 @@
 extends Node3D
 class_name TurretManager
 @export var turret : PackedScene 
-
+@export var enemy_path : Path3D
 func build_turret(newV3) -> void:
 	var newTurret = turret.instantiate()
 	add_child(newTurret)
 	newTurret.global_position = newV3
+	newTurret.enemy_path = enemy_path
